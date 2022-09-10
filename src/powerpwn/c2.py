c725daa4e66611e5862b4e90ed930bc5d851cc19
mbrg <11074433+mbrg@users.noreply.github.com>
2022-09-10 22:02:17 +0300

    fix fixtures

--- src/powerpwn/c2.py
+++ src/powerpwn/c2.py
@@ -34,7 +34,7 @@ class PowerPwnC2:
         return cmd_res
 
     def _run_cmd(self, arguments_as_dict: dict) -> dict:
-        resp = requests.post(url=self.post_url, json=arguments_as_dict)
+        resp = requests.post(url=self.post_url, json=arguments_as_dict)  # type: ignore
 
         if self.debug:
             print(f"Raw content: {resp.content}")
