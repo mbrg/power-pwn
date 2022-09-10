c82df4f26767eb91f0ca2104e7f4452ddb7821a9
mbrg <11074433+mbrg@users.noreply.github.com>
2022-09-10 22:16:09 +0300

    fix mypy

--- tests/powerpwn_tests/c2_test.py
+++ tests/powerpwn_tests/c2_test.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List, Optional, cast
 
 import pytest
 
@@ -25,10 +25,10 @@ class DummyPowerPwnC2(PowerPwnC2):
         super().__init__(post_url, debug)
         self.command_to_run = command_to_run
 
-    def _run_cmd(self, arguments_as_dict: dict) -> dict:
+    def _run_cmd(self, arguments_as_dict: dict) -> dict:  # type: ignore
         cmd_res = CommandResults.construct(
             is_success=True,
-            agent_run_type=AgentRunType.attended.value,
+            agent_run_type=AgentRunType.attended,
             agent_run_errors=AgentRunErrors.construct(attended_run_error={}, unattended_run_error={}),
         )
 
@@ -47,39 +47,40 @@ class DummyPowerPwnC2(PowerPwnC2):
         else:
             raise ValueError(f"command_to_run has invalid value: {self.command_to_run}.")
 
-        return cmd_res.json()
+        cmd_res_as_dict = cast(dict, cmd_res.json())  # type: ignore
+        return cmd_res_as_dict
 
 
 @pytest.mark.parametrize("exec_env", ["exec_py2", "exec_vb" "exec_js", "exec_ps", "exec_cmd"])
-def test_code_exec(exec_env: str, command: str = ""):
+def test_code_exec(exec_env: str, command: str = "") -> None:
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.CODE_EXEC)
 
     exec_env_command = getattr(c2, exec_env)
     exec_env_command(command=command)
 
 
-def test_ransomware(crawl_depth: str = 0, dirs_to_init_crawl: List[str] = None, encryption_key: str = ""):
+def test_ransomware(crawl_depth: str = "0", dirs_to_init_crawl: Optional[List[str]] = None, encryption_key: str = "") -> None:
     if dirs_to_init_crawl is None:
-        dirs_to_init_crawl = []
+        dirs_to_init_crawl = [""]
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.RANSOMWARE)
     c2.ransomware(crawl_depth=crawl_depth, dirs_to_init_crawl=dirs_to_init_crawl, encryption_key=encryption_key)
 
 
-def test_exfiltration(target_file_path: str = ""):
+def test_exfiltration(target_file_path: str = "") -> None:
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.EXFILTRATION)
     c2.exfiltrate(target_file_path=target_file_path)
 
 
-def test_cleanup():
+def test_cleanup() -> None:
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.CLEANUP)
     c2.cleanup()
 
 
-def test_steal_power_automate_token():
+def test_steal_power_automate_token() -> None:
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.STEAL_POWER_AUTOMATE_TOKEN)
     c2.steal_power_automate_token()
 
 
-def test_steal_cookie(fqdn: str = ""):
+def test_steal_cookie(fqdn: str = "") -> None:
     c2 = DummyPowerPwnC2(post_url=POST_URL, debug=DEBUG, command_to_run=CommandToRunEnum.STEAL_COOKIE)
     c2.steal_cookie(fqdn=fqdn)
