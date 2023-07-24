fab4f9585be40ded466d124451ea3a3fec58d03b
lanasalameh1 <lanas@zenity.io>
2023-07-25 00:10:19 +0300

    revert module

--- src/powerpwn/powerdump/collect/data_collectors/idata_collector.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-import requests
-
-
-class IDataCollector(ABC):
-    @abstractmethod
-    def collect(self, session: requests.Session, env_id: str, output_dir: str) -> None:
-        ...
