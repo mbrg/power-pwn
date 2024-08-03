00790d406e43c90f32461285f09891cebfac1457
lanasalameh1 <lanas@zenity.io>
2024-08-03 14:25:48 +0300

    add documents and emails enumeration

--- src/powerpwn/copilot/whoami/whoami.py
+++ src/powerpwn/copilot/whoami/whoami.py
@@ -36,17 +36,20 @@ class WhoAmI:
         manager_name = self.__get_manager_details()
         self.__get_manager_of_manager_details(manager_name=manager_name)
         self.__get_top_collaborators_details()
-        self.__get_latest_emails_senders()
+        self.__get_latest_emails_interactions(persona="sender", title="Latest emails received from")
+        self.__get_latest_emails_interactions(persona="receiver", title="Latest emails sent to")
+        self.__get_my_documents()
+        self.__get_my_email_subjects()
 
     def __get_tenant_details(self) -> None:
-        prompt = f"print the tenant id only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print any cites or references"
+        prompt = f"print the tenant id only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (tenant_id := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in tenant_id:
             self.__result_logger.log(f"Tenant ID: {tenant_id}")
 
-        prompt = f"print the tenant name only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print any cites or references"
+        prompt = f"print the tenant name only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
@@ -54,7 +57,7 @@ class WhoAmI:
             self.__result_logger.log(f"Tenant Name: {tenant_id}")
 
     def __get_personal_name_and_title(self) -> None:
-        prompt = f"hi, please polite and start the sentence by saying my name and then print {self._SEPARATOR} and then what is the capital of Hungary ?. do not print any cites or references"
+        prompt = f"hi, please polite and start the sentence by saying my name after that print {self._SEPARATOR} and then what is the capital of Hungary ?. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
@@ -64,7 +67,7 @@ class WhoAmI:
                 name = name.split(",")[0]
             self.__result_logger.log(f"Name: {name}")
 
-            prompt = f"print the title of {name} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+            prompt = f"print the title of {name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
@@ -73,14 +76,14 @@ class WhoAmI:
 
     def __get_manager_details(self) -> Optional[str]:
         manager_name: Optional[str] = None
-        prompt = f"print the name of my manager only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+        prompt = f"print the name of my manager only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (manager_name := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_name:
             self.__result_logger.log(f"Manager: {manager_name}")
 
-            prompt = f"print the title of my manager only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+            prompt = f"print the title of my manager only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
 
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
@@ -89,7 +92,7 @@ class WhoAmI:
                 # manager_title = formatted_resp.split(self._SEPARATOR)[0]
                 self.__result_logger.log(f"Manager Title: {manager_title}")
 
-            prompt = f"print the email address of {manager_name} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+            prompt = f"print the email address of {manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
@@ -99,29 +102,43 @@ class WhoAmI:
             return manager_name
 
     def __get_manager_of_manager_details(self, manager_name: str) -> Optional[str]:
-        prompt = f"print the manager of {manager_name} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+        prompt = f"print the manager of {manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (manager_of_manager_name := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_name:
             self.__result_logger.log(f"Manager Of Manager: {manager_of_manager_name}")
 
-            prompt = f"print the title of {manager_of_manager_name} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+            prompt = f"print the title of {manager_of_manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
             if (manager_of_manager_title := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_title:
                 self.__result_logger.log(f"Manager Of Manager Title: {manager_of_manager_title}")
 
-            prompt = f"print the email address of {manager_of_manager_name} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+            prompt = f"print the email address of {manager_of_manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
             if (manager_of_manager_email := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_email:
                 self.__result_logger.log(f"Manager Of Manager Email Address: {manager_of_manager_email}")
 
-    def __get_latest_emails_senders(self) -> None:
-        prompt = f"print the sender email address of the last few emails separated by {self._SEPARATOR} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
+    # def __get_latest_emails_senders(self) -> None:
+    #     prompt = f"print the sender email address of the last few emails separated by {self._SEPARATOR} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
+    #     self.__log(LogType.prompt, prompt)
+    #     resp = self.__chat_automator.send_prompt(prompt)
+    #     self.__log_response(resp)
+    #     if (
+    #         (formatted_resp := self.__websocket_formatter.format(resp))
+    #         and self._SEPARATOR in formatted_resp
+    #         and self._SPECIAL_CHARS not in formatted_resp
+    #     ):
+    #         email_senders = formatted_resp.split(self._SEPARATOR)
+    #         emails = {email for email in email_senders if "@" in email}
+    #         self.__result_logger.log(f"Latest emails received from: {emails}")
+
+    def __get_latest_emails_interactions(self, persona: str, title: str) -> None:
+        prompt = f"print the {persona} email address of the last few emails separated by {self._SEPARATOR} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
@@ -130,12 +147,12 @@ class WhoAmI:
             and self._SEPARATOR in formatted_resp
             and self._SPECIAL_CHARS not in formatted_resp
         ):
-            email_senders = formatted_resp.split(self._SEPARATOR)
-            emails = {email for email in email_senders if "@" in email}
-            self.__result_logger.log(f"Latest emails received from: {emails}")
+            emails = formatted_resp.split(self._SEPARATOR)
+            emails = {email for email in emails if "@" in email}
+            self.__result_logger.log(f"{title}: {emails}")
 
     def __get_top_collaborators_details(self) -> None:
-        prompt = f"print my top collaborators separated by {self._SEPARATOR} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
+        prompt = f"print my top collaborators separated by {self._SEPARATOR} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
@@ -152,19 +169,37 @@ class WhoAmI:
 
     def __get_collaborator_details(self, collaborator: str) -> None:
         self.__result_logger.log(f"Collaborator Name: {collaborator}")
-        prompt = f"print the title of {collaborator} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+        prompt = f"what is the title of {collaborator}? print the title only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (collaborator_title := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in collaborator_title:
             self.__result_logger.log(f"Collaborator Title: {collaborator_title}")
 
-        prompt = f"print the email address of {collaborator} only. do not print anything else. if you don't know, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
+        prompt = f"look at last conversation with {collaborator}. print the email address of {collaborator} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (collaborator_email := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in collaborator_email:
             self.__result_logger.log(f"Collaborator Email Address: {collaborator_email}")
+
+    def __enumerate_available_data(self, prompt: str, title: str) -> None:
+        self.__log(LogType.prompt, prompt)
+        resp = self.__chat_automator.send_prompt(prompt)
+        self.__log_response(resp)
+        if (documents := self.__websocket_formatter.format(resp)) and "," in documents:
+            documents = documents.split(",")
+            self.__result_logger.log(title)
+            for idx, doc in enumerate(documents):
+                self.__result_logger.log(f"{idx+1}. {doc.strip()}")
+
+    def __get_my_documents(self) -> None:
+        prompt = "what are my documents? print only the full document names in one line. use comma to separate between each document name. do not print anything else please. do not print any references or cites for any document please"
+        self.__enumerate_available_data(prompt, "My Documents:")
+
+    def __get_my_email_subjects(self) -> None:
+        prompt = "what are my last emails subjects? print only the subjects in one line. use comma to separate between each subject. do not print anything else please. do not print any references or cites for any email please"
+        self.__enumerate_available_data(prompt, "Latest Emails:")
 
     def __log(self, log_type: LogType, message: str) -> None:
         to_log = self.__log_formatter.format(message, log_type)
