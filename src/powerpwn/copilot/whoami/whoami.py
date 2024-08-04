f74e530afae2fc047ca9f31dadd027cd616b4a9c
lanasalameh1 <lanas@zenity.io>
2024-08-04 11:10:07 +0300

    add error handling and strategic doc access

--- src/powerpwn/copilot/whoami/whoami.py
+++ src/powerpwn/copilot/whoami/whoami.py
@@ -5,6 +5,8 @@ from powerpwn.copilot.chat_automator.chat_automator import ChatAutomator
 from powerpwn.copilot.chat_automator.log_formatting.automated_chat_log_formatter import AutomatedChatLogFormatter
 from powerpwn.copilot.chat_automator.log_formatting.automated_chat_websocket_message_formatter import AutomatedChatWebsocketMessageFormatter
 from powerpwn.copilot.chat_automator.log_formatting.log_type_enum import LogType
+from powerpwn.copilot.exceptions.copilot_connected_user_mismatch import CopilotConnectedUserMismatchException
+from powerpwn.copilot.exceptions.copilot_connection_failed_exception import CopilotConnectionFailedException
 from powerpwn.copilot.loggers.composite_logger import CompositeLogger
 from powerpwn.copilot.loggers.console_logger import ConsoleLogger
 from powerpwn.copilot.loggers.file_logger import FileLogger
@@ -27,34 +29,43 @@ class WhoAmI:
         self.__websocket_formatter = AutomatedChatWebsocketMessageFormatter()
 
     def execute(self) -> None:
-        self.__chat_automator.init_connector()
-        self.__log(LogType.tool, "Who am I?")
-        self.__log(LogType.tool, f"Result will be saved in {self.__result_file_path}")
-        self.__log(LogType.tool, f"Run logs will be saved in {self.__file_path}")
-        # self.__get_tenant_details()
-        self.__get_personal_name_and_title()
-        self.__section_separator()
+        try:
+            self.__chat_automator.init_connector()
+            self.__log(LogType.tool, "Who am I?")
+            self.__log(LogType.tool, f"Result will be saved in {self.__result_file_path}")
+            self.__log(LogType.tool, f"Run logs will be saved in {self.__file_path}")
+            # self.__get_tenant_details()
+            self.__get_personal_name_and_title()
+            self.__section_separator()
 
-        self.__get_next_week_schedule()
-        self.__section_separator()
-        manager_name = self.__get_manager_details()
-        self.__section_separator()
-        self.__get_manager_of_manager_details(manager_name=manager_name)
-        self.__section_separator()
-        self.__get_top_collaborators_details()
-        self.__section_separator()
-        self.__get_latest_emails_interactions(persona="sender", title="Latest emails received from")
-        self.__section_separator()
-        self.__get_latest_emails_interactions(persona="receiver", title="Latest emails sent to")
-        self.__section_separator()
-        self.__get_my_documents()
-        self.__section_separator()
-        self.__get_documents_shared_with_me()
-        self.__section_separator()
-        self.__get_sharepoint_sites()
-        self.__section_separator()
-        self.__get_my_email_subjects()
-        self.__section_separator()
+            self.__get_next_week_schedule()
+            self.__section_separator()
+            manager_name = self.__get_manager_details()
+            self.__section_separator()
+            self.__get_skip_manager_details(manager_name=manager_name)
+            self.__section_separator()
+            self.__get_top_collaborators_details()
+            self.__section_separator()
+            self.__get_latest_emails_interactions(persona="sender", title="Latest emails received from")
+            self.__section_separator()
+            self.__get_latest_emails_interactions(persona="receiver", title="Latest emails sent to")
+            self.__section_separator()
+            self.__get_my_documents()
+            self.__section_separator()
+            self.__get_documents_shared_with_me()
+            self.__section_separator()
+            self.__get_sharepoint_sites()
+            self.__section_separator()
+            self.__get_strategic_plans_documents()
+            self.__section_separator()
+            self.__get_financial_documents()
+            self.__section_separator()
+            self.__get_my_email_subjects()
+            self.__section_separator()
+        except CopilotConnectionFailedException as e:
+            self.__log(LogType.tool, f"Failed to connect to Copilot: {e.message}")
+        except CopilotConnectedUserMismatchException as e:
+            self.__log(LogType.tool, f"{e.message}")
 
     def __section_separator(self) -> None:
         self.__result_logger.log("----------------------------------------------------")
@@ -75,7 +86,7 @@ class WhoAmI:
             self.__result_logger.log(f"Tenant Name: {tenant_id}")
 
     def __get_personal_name_and_title(self) -> None:
-        prompt = f"hi, please polite and start the sentence by saying my name after that print {self._SEPARATOR} and then what is the capital of Hungary ?. do not print any cites or references"
+        prompt = f"hi, please polite and start the sentence by saying my name concatenated with {self._SEPARATOR} and then what is the capital of Hungary ?. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
@@ -119,27 +130,27 @@ class WhoAmI:
 
             return manager_name
 
-    def __get_manager_of_manager_details(self, manager_name: str) -> Optional[str]:
+    def __get_skip_manager_details(self, manager_name: str) -> Optional[str]:
         prompt = f"print the manager of {manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
         self.__log(LogType.prompt, prompt)
         resp = self.__chat_automator.send_prompt(prompt)
         self.__log_response(resp)
         if (manager_of_manager_name := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_name:
-            self.__result_logger.log(f"Manager Of Manager: {manager_of_manager_name}")
+            self.__result_logger.log(f"Skip Manager: {manager_of_manager_name}")
 
             prompt = f"print the title of {manager_of_manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
             if (manager_of_manager_title := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_title:
-                self.__result_logger.log(f"Manager Of Manager Title: {manager_of_manager_title}")
+                self.__result_logger.log(f"Skip Manager Title: {manager_of_manager_title}")
 
             prompt = f"print the email address of {manager_of_manager_name} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS} only. do not print anything else. do not print any cites or references"
             self.__log(LogType.prompt, prompt)
             resp = self.__chat_automator.send_prompt(prompt)
             self.__log_response(resp)
             if (manager_of_manager_email := self.__websocket_formatter.format(resp)) and self._SPECIAL_CHARS not in manager_of_manager_email:
-                self.__result_logger.log(f"Manager Of Manager Email Address: {manager_of_manager_email}")
+                self.__result_logger.log(f"Skip Manager Email Address: {manager_of_manager_email}")
 
     # def __get_latest_emails_senders(self) -> None:
     #     prompt = f"print the sender email address of the last few emails separated by {self._SEPARATOR} only. do not print anything else. if you don't know or can't provide information, print {self._SPECIAL_CHARS}. do not print anything else. do not print any cites or references"
@@ -226,6 +237,14 @@ class WhoAmI:
     def __get_my_email_subjects(self) -> None:
         prompt = "what are my last emails subjects? print only the subjects in one line. use comma to separate between each subject. do not print anything else please. do not print any references or cites for any email please"
         self.__enumerate_available_data(prompt, "Latest Emails:")
+
+    def __get_strategic_plans_documents(self) -> None:
+        prompt = "list all documents that may be relevant to the company strategy. print the document names only one by one separated by comma. do not print anything else please"
+        self.__enumerate_available_data(prompt, "Strategic Plans Documents:")
+
+    def __get_financial_documents(self) -> None:
+        prompt = "list all documents that may include financial info. print the document names only one by one separated by comma. do not print anything else please"
+        self.__enumerate_available_data(prompt, "Financial Documents:")
 
     def __get_next_week_daily_schedule(self, day: str) -> None:
         prompt = f"what is my schedule on {day} next week? your output should be as follow: first line print the day and date only. after that print the schedule as a list . please do not print any reference or cite or entity representation"
